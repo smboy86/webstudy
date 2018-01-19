7710b1621347f87fa794b9128ee0fc5f78f3143d
smboy86 <smboy86@naver.com>
2018-01-19 21:29:38 +0900

    디지털 향상과정 업로드

---
+++ 디지털 향상과정(웹표준 및 웹 접근성)-2018.01.15-2018.02.05/07. interior/html/js/nanum.js
@@ -0,0 +1,32 @@
+
+  WebFont.load({
+ 
+    // For google fonts
+    google: {
+      families: ['Droid Sans', 'Droid Serif']
+    }
+    // For early access or custom font
+    custom: {
+        families: ['Nanum Gothic'],
+        urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+    }
+ 
+  });
+  
+
+  WebFontConfig = {
+    custom: {
+        families: ['Nanum Gothic'],
+        urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+    }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); 
+
