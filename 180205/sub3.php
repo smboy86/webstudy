652c385ead7e89297f610a3b91e02f91163ade27
smboy86 <smboy86@naver.com>
2018-02-05 20:36:03 +0900

    180205 강의

---
+++ 180205/sub3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title> meta 요소 삽입하기 </title>
+<meta charset="UTF-8" />
+</head>
+<body>
+	<ul>
+        <li><a href="./sub1.php">회사소개</a></li>
+        <li><a href="./sub2.php">상품들</a></li>
+        <li><a href="./sub3.php">커뮤니티</a></li>
+        <li><a href="./sub4.php">갤러리</a></li>
+    </ul>
+
+    <h1>커뮤니티</h1>
+</body>
+</html>
